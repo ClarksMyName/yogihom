376fa2587db1fac42bccd3cab2be292884fb9bfc
Clark <ryanclark16123@gmail.com>
2026-04-07 20:16:29 -0400

    Update server startup message

--- yogiserver.cjs
+++ yogiserver.cjs
@@ -7,11 +7,8 @@ app.use(express.json());
 
 app.use("/api/instructor", require("./routes/instructorRoutes.cjs"));
 
-
-
 // Start the web server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}...`);
-  console.log('Open http://localhost:8080/index.html in your browser to view the app.');
+  console.log(`Server running on port ${PORT}`);
 });
